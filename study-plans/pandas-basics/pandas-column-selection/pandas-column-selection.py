61f8a7521d343c34a6f9bc7956ae99ecf8a43527
jeanphilippecedricndri-sudo <jeanphilippecedric.ndri@gmail.com>
2026-06-17 22:49:35 +0200

    Solve pandas-column-selection

---
+++ study-plans/pandas-basics/pandas-column-selection/pandas-column-selection.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def select_column(data, column):
+    """
+    Returns: dict with 'values' (list) and 'length' (int)
+    """
+    df = pd.DataFrame(data)
+    col = df[column]
+    return {
+        "values": col.tolist(),
+        "length": len(col)
+    }
